f792027da31796de41a3082daae783afb23370b5
Pakhlavon <46715372+Pakhlavon@users.noreply.github.com>
2023-07-13 23:10:21 +0500

    state

--- app/src/main/java/com/example/newtestapp/MainActivity.kt
+++ app/src/main/java/com/example/newtestapp/MainActivity.kt
@@ -5,12 +5,16 @@ import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -23,45 +27,82 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.newtestapp.ui.theme.NewTestAppTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color(0xFF101010))
-                ) {
-                    Text(
-                        text = buildAnnotatedString {
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.Green,
-                                    fontSize = 50.sp
-                                )
-                            ){
-                                append("J")
-                            }
-                            append("etpack")
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.Green,
-                                    fontSize = 50.sp
-                                )
-                            ){
-                                append("C")
-                            }
-                            append("ompose")
-                        },
-                        color = Color.White,
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.Bold,
-                        fontStyle = FontStyle.Italic,
-                        textAlign = TextAlign.Center,
-                        textDecoration = TextDecoration.Underline
-                    )
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
                 }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ){
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
+                )
+            }
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(Color(0xFF101010))
+//                ) {
+//                    Text(
+//                        text = buildAnnotatedString {
+//                            withStyle(
+//                                style = SpanStyle(
+//                                    color = Color.Green,
+//                                    fontSize = 50.sp
+//                                )
+//                            ){
+//                                append("J")
+//                            }
+//                            append("etpack")
+//                            withStyle(
+//                                style = SpanStyle(
+//                                    color = Color.Green,
+//                                    fontSize = 50.sp
+//                                )
+//                            ){
+//                                append("C")
+//                            }
+//                            append("ompose")
+//                        },
+//                        color = Color.White,
+//                        fontSize = 30.sp,
+//                        fontWeight = FontWeight.Bold,
+//                        fontStyle = FontStyle.Italic,
+//                        textAlign = TextAlign.Center,
+//                        textDecoration = TextDecoration.Underline
+//                    )
+//                }
         }
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updatecolor: (Color) -> Unit
+){
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .clickable {
+                updatecolor(Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                ))
+            }
+    ){}
 }
